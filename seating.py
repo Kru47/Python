122731185c5633d19286daed98b3af75bb0608cc
Kru47 <krutarthghadge47@gmail.com>
2017-12-12 13:06:40 +0530

    Update seating.py
    Akash and Vishal are quite fond of travelling. They mostly travel by railways. They were travelling in a train one day and they got interested in the seating arrangement of their compartment. The compartment looked something like 
    
    
    So they got interested to know the seat number facing them and the seat type facing them. The seats are denoted as follows : 
    
    Window Seat : WSMiddle Seat : MSAisle Seat : AS
    
    You will be given a seat number, find out the seat number facing you and the seat type, i.e. WS, MS or AS.
    
    INPUT
    First line of input will consist of a single integer T denoting number of test-cases. Each test-case consists of a single integer N denoting the seat-number.
    
    OUTPUT
    For each test case, print the facing seat-number and the seat-type, separated by a single space in a new line.
    
    CONSTRAINTS
    1<=T<=1051<=N<=108

--- seating.py
+++ seating.py
@@ -1,4 +1,10 @@
-Stype=["WS","MS","AS","AS","MS","WS","WS","MS","AS","AS","MS","WS"]
+
+              
+     
+    
+                
+                
+               Stype=["WS","MS","AS","AS","MS","WS","WS","MS","AS","AS","MS","WS"]
 Num=int(input("Enter n: "))
 Sno=[]
 TypeOfSeat=[]
@@ -11,9 +17,13 @@ for i in range(0,Num):
         if SnoIp>i and SnoIp<=j:
            if SnoIp<=12:
                if SnoIp<=(i+j)/2:
-                  print(12-SnoIp+1,Stype[-SnoIp])
+                 # print(12-SnoIp+1,Stype[-SnoIp])
+                  Sno.append(12-SnoIp+1)
+                  TypeOfSeat.append(Stype[-SnoIp])
                else:
-                  print(12%SnoIp+1,Stype[-SnoIp])
+                 # print(12%SnoIp+1,Stype[-SnoIp])
+                  Sno.append(12%SnoIp+1)
+                  TypeOfSeat.append(Stype[-SnoIp])
                break
            else:
                if SnoIp<=(i+j)/2:
@@ -22,9 +32,16 @@ for i in range(0,Num):
                else:
                    res=SnoIp%i
                    N=j-res+1
-               print(N,Stype[-res])
+              # print(N,Stype[-res])
+               Sno.append(N)
+               TypeOfSeat.append(Stype[-res])
                break
         i=i+12
         j=j+12
         
-           
+for i in range(0,Num):
+     print(Sno[i]," ",TypeOfSeat[i])       
+
+                   
+               
+   
